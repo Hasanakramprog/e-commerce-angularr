81fa2cebc46e61fd09d0ec713df57e95bbcbaf28
hassan <hassanakranprog@gmail.com>
2024-05-25 18:46:54 +0300

    dummy api

--- src/app/products/service/products.service.ts
+++ src/app/products/service/products.service.ts
@@ -11,32 +11,50 @@ export class ProductsService {
   constructor(private http:HttpClient ) { }
   private user = new BehaviorSubject<any>([]);
   castUser = this.user.asObservable();
-
   getAllProudacts( ){
-    // return this.http.get("https://dummyjson.com/products?limit=28")
-    return this.http.get("http://127.0.0.1:8000/api/all-products/30/0")
+    return this.http.get("https://dummyjson.com/products?limit=28")
   }
   getAllCategories( ){
-    // return this.http.get("https://dummyjson.com/products/categories")
-    return this.http.get("http://127.0.0.1:8000/api/all-categories")
+    return this.http.get("https://dummyjson.com/products/categories")
   }
   getProductByCategories( keword:any){
-    return this.http.get("http://127.0.0.1:8000/api/products/category/"+keword)
+    return this.http.get("https://dummyjson.com/products/category/"+keword)
   }
   SerchProducts( keword:any){
-    return this.http.get("http://127.0.0.1:8000/api/products/serach?q="+keword)
-
+    return this.http.get("https://dummyjson.com/products/search?q="+keword)
   }
   limtProducts( keword:any,skip:any){
-    return this.http.get("http://127.0.0.1:8000/api/all-products/"+keword+"/"+skip)
+    return this.http.get("https://dummyjson.com/products?limit="+keword+"&skip="+skip)
   }
   GetProductById( id:any){
-    // return this.http.get("https://dummyjson.com/products/"+id)
-    return this.http.get("http://127.0.0.1:8000/api/products/"+id)
+    return this.http.get("https://dummyjson.com/products/"+id)
   }
 
-  OrderProductById( data:any){
-    // return this.http.get("https://dummyjson.com/products/"+id)
-    return this.http.post("http://127.0.0.1:8000/api/products/order/",data)
-  }
+  // getAllProudacts( ){
+  //   // return this.http.get("https://dummyjson.com/products?limit=28")
+  //   return this.http.get("http://127.0.0.1:8000/api/all-products/30/0")
+  // }
+  // getAllCategories( ){
+  //   // return this.http.get("https://dummyjson.com/products/categories")
+  //   return this.http.get("http://127.0.0.1:8000/api/all-categories")
+  // }
+  // getProductByCategories( keword:any){
+  //   return this.http.get("http://127.0.0.1:8000/api/products/category/"+keword)
+  // }
+  // SerchProducts( keword:any){
+  //   return this.http.get("http://127.0.0.1:8000/api/products/serach?q="+keword)
+
+  // }
+  // limtProducts( keword:any,skip:any){
+  //   return this.http.get("http://127.0.0.1:8000/api/all-products/"+keword+"/"+skip)
+  // }
+  // GetProductById( id:any){
+  //   // return this.http.get("https://dummyjson.com/products/"+id)
+  //   return this.http.get("http://127.0.0.1:8000/api/products/"+id)
+  // }
+
+  // OrderProductById( data:any){
+  //   // return this.http.get("https://dummyjson.com/products/"+id)
+  //   return this.http.post("http://127.0.0.1:8000/api/products/order/",data)
+  // }
 }
