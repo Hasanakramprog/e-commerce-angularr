7fac2f7837b49827caffd23cfecab94dedb3502d
hassan <hassanakranprog@gmail.com>
2024-06-03 18:27:50 +0300

    enable rating star and fix rounding price

--- src/app/products/component/product/product.component.ts
+++ src/app/products/component/product/product.component.ts
@@ -14,6 +14,12 @@ export class ProductComponent implements OnInit {
   amount: number = 1
   ngOnInit(): void {
   }
+
+  onRateChange(index:any){
+    console.log(index)
+    console.log(this.data.id)
+
+  }
   // get single proudct information
   add() {
     this.item.emit({ item:this.data, quantity: this.amount })
@@ -23,7 +29,11 @@ export class ProductComponent implements OnInit {
   calculateDiscountPrice(price: number, discountPercent: number) {
     if (discountPercent > 0) {
       const discountAmount = price * (discountPercent / 100);
-      return price - discountAmount;
+      let discountPrice= price - discountAmount;
+      let discountPriceString=discountPrice.toFixed(2);
+      discountPrice=+discountPriceString;
+
+      return discountPrice;
     } else {
       return price; // No discount applied
     }
